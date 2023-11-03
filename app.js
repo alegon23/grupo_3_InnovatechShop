bd1286d3c9b71684dfa9c117f86683d13192ff55
Alejandra <alegonzalez236@gmail.com>
2023-11-03 20:43:47 -0300

    agregando rutas

--- app.js
+++ app.js
@@ -6,9 +6,27 @@ const app = express()
 const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
+
 app.listen(PORT, () => {
     console.log('Servidor corriendo en el puerto '+PORT);
-})
+});
+
 app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './views/index.html'))
-})
+});
+
+app.get('/login', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
+
+app.get('/registro', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/registro.html'))
+});
+
+app.get('/carrito', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/carrito.html'))
+});
+
+app.get('/detalleProducto', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/detalleProducto.html'))
+});
