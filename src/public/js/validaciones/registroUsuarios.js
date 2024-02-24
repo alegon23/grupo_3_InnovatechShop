53d63396a776a6b171d47d3ee4bf7e903e0b5926
Alejandra <alegonzalez236@gmail.com>
2024-02-24 16:55:22 -0300

    validacion front registro usuario

---
+++ src/public/js/validaciones/registroUsuarios.js
@@ -0,0 +1,200 @@
+const inputValidations = [
+    {
+        inputName: "nombre",
+        type: ["onTime", "submit"],
+        validations: [
+            {
+                validator: (input) => !validator.isEmpty(input),
+                errorMsg: "El nombre es obligatorio"
+            },
+            {
+                validator: (input) => validator.isLength(input, {min: 2}),
+                errorMsg: "El nombre es muy corto"
+            }
+        ]
+    },
+    {
+        inputName: "apellido",
+        type: ["onTime", "submit"],
+        validations: [
+            {
+                validator: (input) => !validator.isEmpty(input),
+                errorMsg: "El apellido es obligatorio"
+            },
+            {
+                validator: (input) => validator.isLength(input, {min: 2}),
+                errorMsg: "El apellido es muy corto"
+            }
+        ]
+    },
+    {
+        inputName: "email",
+        type: ["onTime", "submit"],
+        validations: [
+            {
+                validator: (input) => !validator.isEmpty(input),
+                errorMsg: "El email es obligatorio"
+            },
+            {
+                validator: (input) => validator.isEmail(input),
+                errorMsg: "El email tiene un formato incorrecto"
+            },
+            {
+                validator: async (input) => {
+                    const res = await fetch(`/users/validate/${input}`)
+                    const data = await res.json()
+                    return !data.existe
+                },
+                errorMsg: "El email ya existe"
+            }
+        ]
+    },
+    {
+        inputName: "contrasenia",
+        type: ["onTime", "submit"],
+        validations: [
+            {
+                validator: (input) => !validator.isEmpty(input),
+                errorMsg: "La contraseña es obligatoria"
+            },
+            {
+                validator: (input) => validator.isLength(input, {min: 8}),
+                errorMsg: "La contraseña debe tener al menos 8 caracteres"
+            },
+            {
+                validator: (input) => validator.isStrongPassword(input, { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 0, returnScore: false }),
+                errorMsg: "La contraseña debe al menos 1 mayúscula, 1 minúscula y 1 número"
+            }
+        ]
+    },
+    {
+        inputName: "fecha",
+        type: ["onTime", "submit"],
+        validations: [
+            {
+                //por ejemplo poner 31/02
+                validator: (input) => validator.isISO8601(input),
+                errorMsg: "La fecha no tiene formato valido"
+            },
+            {
+                //controla que no se pongan fechas mayores a las de hoy
+                validator: (input) => !validator.isAfter(input),
+                errorMsg: "La fecha no es valida"
+            },
+            /*{
+                validator: (input) => !validator.isEmpty(input),
+                errorMsg: "La fecha es obligatoria"
+            },*/
+        ]
+    },
+    {
+        inputName: "confirmarContrasenia",
+        type: ["onTime", "submit"],
+        validations: [
+            {
+                validator: (input) => !validator.isEmpty(input),
+                errorMsg: "Debes confirmar la contraseña"
+            },
+        ]
+    },
+    {
+        inputName: "avatar",
+        type: ["submit"],
+        validations: [
+            {
+                validator: (input) => {
+                    const fileExtension = input.split(".")[1];
+                    if (fileExtension) {
+                        const acceptedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+                        return acceptedExtensions.includes(fileExtension)
+                    } else{ 
+                        return true
+                    }
+                },
+                errorMsg: "Las extensiones de archivo permitidas son .jpg, .jpeg, .png, .gif"
+            },
+        ]
+    },
+]
+
+//* VALIDACION ON-TIME
+window.addEventListener("load", function () {
+    const form = document.querySelector("form.contenedor-registro");
+
+    inputValidations.forEach((inputToValidate) => {
+        if(inputToValidate.type.includes("onTime")) {
+            const input = form[inputToValidate.inputName];
+    
+            const inputContainer = input.parentElement;
+            
+            input.addEventListener("keyup", async function (e) {
+                for (const validation of inputToValidate.validations) {
+    
+                    const isValid = await validation.validator(e.target.value);
+    
+                    if (!isValid) {
+                        inputContainer.querySelector(".error").innerHTML = validation.errorMsg;
+                        break
+                    } else {
+                        inputContainer.querySelector(".error").innerHTML = "";
+                    }
+                }
+            })
+        }
+    });
+
+    //* valida que confirmar contraseña es igual a la contraseña
+    const inputConfirm = form["confirmarContrasenia"];
+    const inputConfirmContainer = inputConfirm.parentElement;
+    const inputPass = form["contrasenia"];
+
+    inputConfirm.addEventListener("keyup", function(e) {
+        if (inputPass.value != inputConfirm.value) {
+            inputConfirmContainer.querySelector(".error").innerHTML = "No coincide con la contraseña";
+        }
+    })
+
+    //* VALIDACION EN SUBMIT
+    form.addEventListener("submit", function (e) {
+        //no se envia formulario
+        e.preventDefault();
+
+        //array de errores
+        const errores = [];
+
+        //por cada objeto de inputValidations
+        inputValidations.forEach((inputToValidate) => {
+            if(inputToValidate.type.includes("submit")) {
+                //se obtiene input html
+                const input = form[inputToValidate.inputName];
+    
+                //se obtiene padre de input - en este caso seria el div
+                const inputContainer = input.parentElement;
+    
+                //por cada objeto de inputValidations, se cicla sobre el array de validaciones
+                for (const validation of inputToValidate.validations) {
+                    //se aplica el validador sobre el valor actual del input
+                    const isValid = validation.validator(input.value);
+    
+                    //si es invalido -> muestra error + guarda en array
+                    if (!isValid) {
+                        errores.push(validation.errorMsg);
+                        inputContainer.querySelector(".error").innerHTML = validation.errorMsg;
+                        return;
+                    }
+                }
+    
+                inputContainer.querySelector(".error").innerHTML = "";
+            }
+        });
+
+        //si no hay errores, envia el form. Si hay errores, muestra mensaje
+        if (errores.length == 0) {
+            form.submit();
+        } else {
+            const spanErrorSubmit = document.querySelector("span.errorSubmit");
+            spanErrorSubmit.innerHTML = "Completa correctamente todos los campos"
+            spanErrorSubmit.classList.add("error-submit")
+        }
+    })
+});
