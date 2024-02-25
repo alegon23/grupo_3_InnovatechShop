f5a909a73a766f8ac589042913b4eaef2914ff42
Nahuel417 <Banconahuel417@gmail.com>
2024-02-25 16:50:07 -0300

    validacion front del crear producto

--- src/middleware/validatorProd.js
+++ src/middleware/validatorProd.js
@@ -4,8 +4,8 @@ const path = require('path');
 const validacionesProducto = [
     body('nombre').notEmpty().withMessage("Debes ingresar el nombre del producto").bail().isLength({ min:5 }).withMessage('El nombre debe tener al menos 5 caracteres'),
     body('marca').notEmpty().withMessage("Debes ingresar la marca del producto"),
-    body('categoria').notEmpty().withMessage("Debes seleccionar una categoría"),
-    body('precio').notEmpty().withMessage("Debes ingresar el precio del producto").bail().isNumeric({min: 0}).withMessage('El precio debe ser un numero positivo'),
+    body('categoria').notEmpty().withMessage("Debes elegir una categoría"),
+    body('precio').notEmpty().withMessage("Debes ingresar el precio del producto").bail().isNumeric({min: 1}).withMessage('El precio debe ser un numero positivo'),
     body('stock').notEmpty().withMessage("Debes ingresar el stock del producto").bail().isInt({min: 0}).withMessage('El stock debe ser mayor o igual a cero'),
     body('porcentaje').notEmpty().withMessage("Debes ingresar el porcentaje de descuento").bail().isInt({min: 0}).withMessage('El porcentaje debe ser mayor o igual a cero'),
     body('descripcion').isLength({ min: 20 }).withMessage('La descripcion debe tener al menos 20 caracteres'),
