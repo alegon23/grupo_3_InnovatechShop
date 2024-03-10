7830183332d6d47d3c19c331c6aacf519f67470b
MirianBaigorria <mirianbaigo@gmail.com>
2024-03-10 17:59:00 -0300

    dashboard topPaneles y ultimo producto

--- dashboard/src/components/LastProduct.jsx
+++ dashboard/src/components/LastProduct.jsx
@@ -1,13 +1,41 @@
 import logo from "../assets/images/Logo.png";
 
-const LastProduct = () => {
-  const lastProduct = {
-    imagen: logo,
-    alt: "Logo",
-    descripcion:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?",
-  };
+import {useState, useEffect} from 'react';
 
+const LastProduct = () => {
+ 
+
+  const [lastProduct, setLastProduct] = useState({});
+
+    useEffect(() => {
+        const obtenerLastProduct = async () => {
+          try {
+            const res = await fetch(
+              `http://localhost:8080/api/products`
+            );
+            const data = await res.json();
+       
+            const last=data.products[data.products.length-1];
+
+            console.log(last);
+
+            const resDetalle = await fetch(
+              `http://localhost:8080/api/products/${last.idProduct}`
+            );
+            const dataDetalle = await resDetalle.json();
+
+            console.log(dataDetalle);
+            setLastProduct(dataDetalle.product || {})
+            
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        
+        obtenerLastProduct();
+
+       
+      }, []);
   return (
     <div className="col-lg-6 mb-4">
       <div className="card shadow mb-4">
@@ -16,19 +44,21 @@ const LastProduct = () => {
             Último producto en la Base de Datos
           </h5>
         </div>
+        {lastProduct &&  
         <div className="card-body">
           <div className="text-center">
             <img
               className="img-fluid px-3 px-sm-4 mt-3 mb-4"
-              src={lastProduct.imagen}
-              alt={lastProduct.alt}
-            />
+              src={lastProduct.imagenes ? lastProduct.imagenes[0] : logo}
+              alt={lastProduct.productName}
+        /> 
+           
           </div>
-          <p>{lastProduct.descripcion}</p>
+          <p>{lastProduct.description}</p>
           <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">
             Ver detalle del producto
           </a>
-        </div>
+        </div>}
       </div>
     </div>
   );
--- dashboard/src/components/Panels.jsx
+++ dashboard/src/components/Panels.jsx
@@ -4,12 +4,56 @@ import LastProduct from "./LastProduct"
 import * as Icon from 'react-bootstrap-icons';
 import ProductsList from "./ProductsList";
 
+import {useState, useEffect} from 'react';
+
 
 const Panels = () => {
+  const [cantProduct,setCantProduct] = useState(0);
+
+  const [cantUser,setCantUser] = useState(0);
+
+  const [cantCategory,setCategory] = useState(0);
+
+
+    useEffect(() => {
+        const obtenerCantidadProduct= async () => {
+          try {
+            const res = await fetch(
+              `http://localhost:8080/api/products`
+            );
+            const data = await res.json();
+            setCantProduct(data.meta.count || 0);
+            setCategory(data.meta.countByCategory.length || 0 );
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        obtenerCantidadProduct();
+
+        const obtenerCantidadUser= async () => {
+          try {
+            const res = await fetch(
+              `http://localhost:8080/api/users`
+            );
+            const data = await res.json();
+
+            setCantUser(data.meta.count || 0 );
+          } catch (error) {
+            console.log(error);
+          }};
+
+          obtenerCantidadUser();
+
+
+      }, []);
+
+
+
+
   const topPanels = [
-    { titulo: "Total de Productos", cifra: 21, colorBorde: "info", icon: <Icon.Box size={35} color="rgba(0,0,0,0.2)"/>},
-    { titulo: "Total de Usuarios", cifra: 79, colorBorde: "danger", icon: <Icon.People size={35} color="rgba(0,0,0,0.2)"/>},
-    { titulo: "Total de Categorías", cifra: 49, colorBorde: "warning", icon: <Icon.Grid size={35} color="rgba(0,0,0,0.2)"/>}
+    { titulo: "Total de Productos", cifra: cantProduct, colorBorde: "info", icon: <Icon.Box size={35} color="rgba(0,0,0,0.2)"/>},
+    { titulo: "Total de Usuarios", cifra: cantUser, colorBorde: "danger", icon: <Icon.People size={35} color="rgba(0,0,0,0.2)"/>},
+    { titulo: "Total de Categorías", cifra: cantCategory, colorBorde: "warning", icon: <Icon.Grid size={35} color="rgba(0,0,0,0.2)"/>}
   ]
 
   return (
